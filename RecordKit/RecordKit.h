41e4a9b85c096c8980f0b3e6953291878e3d1123
Masaaki Goshima <goccy54@gmail.com>
2017-06-14 18:45:44 +0900

    add project templates

---
+++ RecordKit/RecordKit.h
@@ -0,0 +1,19 @@
+//
+//  RecordKit.h
+//  RecordKit
+//
+//  Created by goccy on 2017/06/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RecordKit.
+FOUNDATION_EXPORT double RecordKitVersionNumber;
+
+//! Project version string for RecordKit.
+FOUNDATION_EXPORT const unsigned char RecordKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecordKit/PublicHeader.h>
+
+
