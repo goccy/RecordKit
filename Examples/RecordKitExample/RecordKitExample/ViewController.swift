ce2a4d5936099c061c68f8c6b541f82a65df79ce
Masaaki Goshima <goccy54@gmail.com>
2017-06-15 11:57:14 +0900

    add Example project

---
+++ Examples/RecordKitExample/RecordKitExample/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  RecordKitExample
+//
+//  Created by goccy on 2017/06/14.
+//  Copyright © 2017年 goccy. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: AVPlayerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.playMovieFromProjectBundle()
+    }
+    
+    func playMovieFromProjectBundle() {
+        if let bundlePath = Bundle.main.path(forResource: "cat", ofType: "mp4") {
+            playMovie(url : URL(fileURLWithPath: bundlePath))
+        } else {
+            print("no such file")
+        }
+    }
+    
+    func playMovie(url :URL) {
+        let videoPlayer = AVPlayer(url: url)
+        self.player = videoPlayer
+        videoPlayer.play()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
